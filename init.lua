3320430a107e5074396da290d3f3861bddf63e5d
Shad MOrdre <shadmordre@gmail.com>
2019-09-05 12:35:20 -0700

    Add files via upload

--- init.lua
+++ init.lua
@@ -18,9 +18,30 @@ minetest.log(S("[MOD] lib_trm:  Loading..."))
 minetest.log(S("[MOD] lib_trm:  Legal Info: Copyright " .. lib_trm.copyright .. " " ..lib_trm.authorship))
 minetest.log(S("[MOD] lib_trm:  License: " .. lib_trm.license))
 
+	
+	dofile(lib_trm.path_mod.."/lib_trm_toolcap_modifier.lua")
+	dofile(lib_trm.path_mod.."/lib_trm_tool_ranks.lua")
+	
+	
+	--minetest.register_on_mods_loaded(
+	--	if minetest.global_exists("default") and minetest.get_modpath("default") then
+	--		dofile(MP.."/lib_trm_default_support.lua")
+	--	end
+	--)
 
-dofile(lib_trm.path_mod.."/lib_trm_toolcap_modifier.lua")
-dofile(lib_trm.path_mod.."/lib_trm_tool_ranks.lua")
+	minetest.register_on_mods_loaded(function()
+		for _, node_name in pairs(minetest.registered_nodes) do
+			local node_def = minetest.registered_nodes[node_name]
+			if node_def.tool_capabilities then
+				minetest.override_item(node_name, {
+					original_description = "Diamond Pickaxe",
+					description = toolranks.create_description("Diamond Pickaxe", 0, 1),
+					after_use = toolranks.new_afteruse
+				})
+			end
+		end
+	end
+
 
 
 minetest.log(S("[MOD] lib_trm:  Successfully loaded."))

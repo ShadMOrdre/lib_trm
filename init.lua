62650f28e2d65fd0758d1c4086827ff570e745c1
Shad MOrdre <shadmordre@gmail.com>
2019-09-06 15:14:18 -0700

    Add files via upload

--- init.lua
+++ init.lua
@@ -14,34 +14,36 @@ lib_trm.path_world = minetest.get_worldpath()
 lib_trm.intllib = minetest.get_translator(lib_trm.name)
 local S = lib_trm.intllib
 
-minetest.log(S("[MOD] lib_trm:  Loading..."))
-minetest.log(S("[MOD] lib_trm:  Legal Info: Copyright " .. lib_trm.copyright .. " " ..lib_trm.authorship))
-minetest.log(S("[MOD] lib_trm:  License: " .. lib_trm.license))
+--minetest.log(S("[MOD] lib_trm:  Loading..."))
+--minetest.log(S("[MOD] lib_trm:  Legal Info: Copyright " .. lib_trm.copyright .. " " ..lib_trm.authorship .. ""))
+--minetest.log(S("[MOD] lib_trm:  License: " .. lib_trm.license .. ""))
+
+minetest.log("[MOD] lib_trm:  Loading...")
+minetest.log("[MOD] lib_trm:  Legal Info: Copyright " .. lib_trm.copyright .. " " ..lib_trm.authorship .. "")
+minetest.log("[MOD] lib_trm:  License: " .. lib_trm.license .. "")
 
 	
 	dofile(lib_trm.path_mod.."/lib_trm_toolcap_modifier.lua")
 	dofile(lib_trm.path_mod.."/lib_trm_tool_ranks.lua")
 	
 	
-	--minetest.register_on_mods_loaded(
-	--	if minetest.global_exists("default") and minetest.get_modpath("default") then
-	--		dofile(MP.."/lib_trm_default_support.lua")
-	--	end
-	--)
 
 	minetest.register_on_mods_loaded(function()
-		for _, node_name in pairs(minetest.registered_nodes) do
-			local node_def = minetest.registered_nodes[node_name]
-			local node_desc = node_def.description
-			if node_def.tool_capabilities then
-				minetest.override_item(node_name, {
-					original_description = node_desc,
-					description = toolranks.create_description(node_desc, 0, 1),
-					after_use = toolranks.new_afteruse
-				})
+		for node_name, node_def in pairs(minetest.registered_tools) do
+			if node_name and node_name ~= "" then
+				if node_def then
+					if not node_def.original_description then
+						local node_desc = node_def.description
+						minetest.override_item(node_name, {
+							original_description = node_desc,
+							description = toolranks.create_description(node_desc, 0, 1),
+							after_use = toolranks.new_afteruse,
+						})
+					end
+				end
 			end
 		end
-	end
+	end)
 
 
 
